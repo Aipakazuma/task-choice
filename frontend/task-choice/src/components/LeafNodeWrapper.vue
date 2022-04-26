df9c716d242855c87075c3fd1f23a50dccc2e371
aipa <k.tamashiro@churadata.okinawa>
2022-04-27 00:46:21 +0900

    [modify] 追加、削除できるように修正.

--- frontend/task-choice/src/components/LeafNodeWrapper.vue
+++ frontend/task-choice/src/components/LeafNodeWrapper.vue
@@ -4,28 +4,39 @@ import LeafNode from "@/components/LeafNode.vue";
 
 const items = ref<Array<string>>(["Java", "Perl", "PHP"]);
 const submit = (e) => {
+  if (e.target.value === "") {
+    return;
+  }
   items.value.push(e.target.value);
   e.target.value = "";
+};
+const deleteEvent = (targetIndex) => {
+  items.value = items.value.filter((v, index) => {
+    return index !== targetIndex;
+  });
 };
 </script>
 
 <template>
   <div>
-    <input type="text" @keyup.enter="submit" />
+    <input type="text" @keyup.enter="submit" placeholder="入力してください" />
     <ul>
-      <li v-for="item in items" :key="item">
+      <li v-for="(item, index) in items" :key="item">
         <leaf-node :name="item" />
+        <button @click="deleteEvent(index)" class="delete-button">×</button>
       </li>
     </ul>
   </div>
 </template>
 
 <style scoped>
-button {
-  display: block;
-  width: 100%;
-  margin-top: 20px;
-  padding: 5px 0;
+input {
+  padding: 10px 0;
+  border: 1px solid;
+  margin-top: 10px;
+  padding-left: 14px;
+  border-radius: 5px;
+  margin-left: auto;
 }
 
 ul {
@@ -41,5 +52,19 @@ li {
   border: 1px solid #000;
   padding: 5px;
   margin: 10px 10px;
+  overflow-wrap: break-word;
+  position: relative;
+}
+
+.delete-button {
+  position: absolute;
+  top: -10px;
+  right: -10px;
+  background: #fff;
+  border-radius: 50%;
+  border: 1px solid #000;
+  font-size: 10px;
+  width: 20px;
+  height: 20px;
 }
 </style>
